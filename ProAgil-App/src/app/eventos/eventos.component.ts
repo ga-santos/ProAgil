f3c1ed6668d98e8eef994dfb29f8868a67714853
Gabriel Santos <gabriel.dos.santos.gs@gmail.com>
2020-12-15 23:48:39 -0300

    Adicionando Angular

---
+++ ProAgil-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,44 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.css']
+})
+export class EventosComponent implements OnInit {
+
+  
+  /*
+  eventos: any = [
+    {
+      EventoId: 1
+      ,Tema: "Angular"
+      ,Local: "São Paulo"
+    },
+    {
+      EventoId: 2
+      ,Tema: ".NET"
+      ,Local: "São Bernardo"
+    }
+  ]
+  */
+
+  eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getEventos();
+  }
+
+  getEventos(){
+    this.http.get("http://localhost:5000/api/values").subscribe(response => {
+      this.eventos = response;
+    }, error => {
+      console.log(error);
+    }
+    );
+  }
+
+}
